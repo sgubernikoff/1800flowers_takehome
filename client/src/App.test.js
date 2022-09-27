6e6d2c20932c0af6b2dcd678cf7bc0c1731a84aa
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-09-27 14:35:03 -0400

    unit testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,54 +4,55 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import { unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
 
 const render = (component) =>
-  rtlRender(<Provider store={store}>{component}</Provider>);
+  rtlRender(
+    <Provider store={store}>
+      <BrowserRouter>{component}</BrowserRouter>
+    </Provider>
+  );
 
 test("renders App component", () => {
   render(<App />);
 });
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+// let container = null;
+// beforeEach(() => {
+//   // setup a DOM element as a render target
+//   container = document.createElement("div");
+//   document.body.appendChild(container);
+// });
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+// afterEach(() => {
+//   // cleanup on exiting
+//   unmountComponentAtNode(container);
+//   container.remove();
+//   container = null;
+// });
 
-it("renders post data", async () => {
-  const fakePosts = [
-    {
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-      id: 1,
-      title: "sunt",
-      userId: 1,
-    },
-  ];
-  jest.spyOn(global, "fetch").mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(fakePosts),
-    })
-  );
+// it("getPosts is triggered", async () => {
+//   const fakePosts = [
+//     {
+//       body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+//       id: 1,
+//       title: "sunt",
+//       userId: 1,
+//     },
+//   ];
+//   jest.spyOn(global, "fetch").mockImplementation(() =>
+//     Promise.resolve({
+//       json: () => Promise.resolve(fakePosts),
+//     })
+//   );
 
-  // Use the asynchronous version of act to apply resolved promises
-  await act(async () => {
-    render(<App />, container);
-  });
+//   // Use the asynchronous version of act to apply resolved promises
+//   await act(async () => {
+//     render(<App />, container);
+//   });
 
-  expect(
-    document
-      .querySelector(".title_hold")
-      .children[0].innerHTML.includes(fakePosts[0].title)
-  );
+//   // expect(getPosts.toHaveBeenCalled());
 
-  // remove the mock to ensure tests are completely isolated
-  global.fetch.mockRestore();
-});
+//   // remove the mock to ensure tests are completely isolated
+//   global.fetch.mockRestore();
+// });
